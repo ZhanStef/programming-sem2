d8661f3a9410200d7318b0481711378680a7dded
JeanStef <jeanstefanovich@gmail.com>
2017-05-25 02:27:14 +0700

    Kurs_work not finished but near

--- kurs work/kurs_work.c
+++ kurs work/kurs_work.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #define DLINA_STR_SLOVA 20
 #define SCHITIVAEM 80
-#define BUCKVA_OR_CIFRA(X) ((X>=34 && X<=90) || (X>=48 && X<=57) || (X>=97 && X<=122))
+#define BUCKVA_OR_CIFRA(X) ((X>=65 && X<=90) || (X>=48 && X<=57) || (X>=97 && X<=122))
 #define RAZDELITEL_PREDLOJENII(X) (X=='?' || X=='!' || X=='.')
 #define ESHE_SLOVO(X) X=(sl *)malloc(sizeof(sl)); tail->next=X; tail=X;
 
@@ -17,7 +17,8 @@ void ZapolnenieOcheredi(sl *tail);
 void VivodOcheredi(sl * head, sl *tail);
 
 int main(){
-    sl *head=(sl *) malloc(sizeof(sl)), *tail=(sl*)&head;
+    sl *head=(sl *) malloc(sizeof(sl));
+    sl *tail=head;
     ZapolnenieOcheredi(tail);
     VivodOcheredi(head, tail);
     return 0;
@@ -25,17 +26,14 @@ int main(){
 
 void ZapolnenieOcheredi(sl *tail){
     FILE *f1=fopen("file.txt","r");
-    int g=0, flag=1;
+    int g=0;
     unsigned int i;
     sl *p=tail;
+    p->flag=1;
     char Arr[SCHITIVAEM];
     while(fgets(Arr, SCHITIVAEM, f1)){
-        for(i=0; i<strlen(Arr); i++){
+        for(i=0; i<strlen(Arr);){
                 if(g==0){
-                    if(BUCKVA_OR_CIFRA(Arr[i]) && flag!=0){
-                        p->flag=1;
-                        flag=0;
-                    }
                     p->str[g++]=Arr[i++];
                 }
                 while (BUCKVA_OR_CIFRA(Arr[i]) && g!=DLINA_STR_SLOVA){
@@ -56,8 +54,9 @@ void ZapolnenieOcheredi(sl *tail){
                     }
                 }
                 if(RAZDELITEL_PREDLOJENII(Arr[i])){
-                    flag=1;
+                    p->flag=1;
                     ESHE_SLOVO(p);
+                    p->flag=1;
                     g=0;
                 }
                 else{
@@ -75,10 +74,25 @@ void VivodOcheredi(sl * head, sl *tail){
     int i;
     while (p!=tail) {
         for(i=0;i<20;i++){
-            putchar(p->str[i]);
+            if(p->str[i]!=0){
+				putchar(p->str[i]);
+			}
+			else{
+				putchar(' ');
+				break;
+			}
         }
         p=p->next;
     }
-    printf("%s", p->str);
+	for(i=0;i<20;i++){
+		if(p->str[i]!=0){
+			putchar(p->str[i]);
+		}
+		else{
+			putchar(' ');
+			break;
+		}
+	}
+	putchar(0);
 }
 
