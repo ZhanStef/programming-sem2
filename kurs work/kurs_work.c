59ff295bc5a926c5c1e23a6ed26216b8e194b9b3
JeanStefan <jeanstefanovich@gmail.com>
2017-05-27 02:01:44 +0700

    Refactoring in progress

--- kurs work/kurs_work.c
+++ kurs work/kurs_work.c
@@ -1,10 +1,11 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define DLINA_STR_SLOVA 20
+#define DLINA_STR_SLOVA 60
 #define SCHITIVAEM 80
 #define BUCKVA_OR_CIFRA(X) ((X>=65 && X<=90) || (X>=48 && X<=57) || (X>=97 && X<=122))
 #define RAZDELITEL_PREDLOJENII(X) (X=='?' || X=='!' || X=='.')
+#define RAZDELITEL_SLOV(X) (X==' ' || X=='\n')
 #define ESHE_SLOVO(X) X=(sl *)malloc(sizeof(sl)); tail->next=X; tail=X;
 
 typedef struct slovo{
@@ -25,50 +26,62 @@ int main(){
 }
 
 sl * ZapolnenieOcheredi(sl *tail){
-    FILE *f1=fopen("file.txt","r");
+    FILE *f1;
+    if(!(f1=fopen("file.txt","r"))){
+            printf("\nOpen file failure. ");
+    }
     int g=0;
     unsigned int i;
     sl *p=tail;
-    p->flag=1;
+    int flag=1;
     char Arr[SCHITIVAEM];
     while(fgets(Arr, SCHITIVAEM, f1)){
         for(i=0; i<strlen(Arr);){
-                if(g==0){
-                    p->str[g++]=Arr[i++];
-                }
-                while (BUCKVA_OR_CIFRA(Arr[i]) && g!=DLINA_STR_SLOVA){
-                    p->str[g++]=Arr[i++];
-                }
-                if(g==DLINA_STR_SLOVA){
-                    if(p->flag==1){
-                        ESHE_SLOVO(p);
-                        p->flag=1;
-                        g=0;
-                        continue;
-                    }
-                    else{
-                        ESHE_SLOVO(p);
-                        p->flag=0;
-                        g=0;
-                        continue;
-                    }
-                }
-                if(RAZDELITEL_PREDLOJENII(Arr[i])){
-                    p->flag=1;
-                    ESHE_SLOVO(p);
-                    p->flag=1;
-                    g=0;
-                }
-                else{
+            if(RAZDELITEL_SLOV(Arr[i])){
+                if(g!=0){
                     ESHE_SLOVO(p);
                     p->flag=0;
                     g=0;
                 }
+                while(RAZDELITEL_SLOV(Arr[i]) && g<DLINA_STR_SLOVA ){
+                        p->str[g++]=Arr[i];
+                }
+            }
+            if(BUCKVA_OR_CIFRA(Arr[i])){
+                ESHE_SLOVO(p);
+                if(flag==1){
+                    p->flag=1;
+                    flag=0;
+                    g=0;
+                }
+                else{
+                    p->flag=0;
+                    g=0;
+                }
+                while(BUCKVA_OR_CIFRA(Arr[i]) && g!=DLINA_STR_SLOVA){
+                    p->str[g++]=Arr[i];
+                }
+            }
+            if(RAZDELITEL_PREDLOJENII(Arr[i])){
+                p->flag=1;
+                flag=1;
+                ESHE_SLOVO(p);
+                g=0;
+                p->flag=0;
+                while (RAZDELITEL_PREDLOJENII(Arr[i]) && g!=DLINA_STR_SLOVA) {
+                    p->str[g++]=Arr[i];
+                }
+            }
+            if(Arr[i]==0){
+                p->flag=1;
+                ESHE_SLOVO(p);
+                p->flag=0;
+                p->str[0]=0;
+                fclose(f1);
+                return tail;
+            }
         }
     }
-    fclose(f1);
-	return tail;
-}
 
 void VivodOcheredi(sl * head, sl *tail){
     sl *p=head;
@@ -76,35 +89,35 @@ void VivodOcheredi(sl * head, sl *tail){
     while (p!=tail) {
         for(i=0;i<20;i++){
             if(p->str[i]!=0){
-				putchar(p->str[i]);
-			}
-			else{
-				//putchar(' ');
-				break;
-			}
+                putchar(p->str[i]);
+            }
+            else{
+                //putchar(' ');
+                break;
+            }
         }
-		if(p->flag==1){
-			printf("(<=slovo and flag=1) ");
-		}
-		else{
-			printf("(<=slovo and flag=0) ");
-		}
-		p=p->next;
+        if(p->flag==1){
+            printf("(<=slovo and flag=1) ");
+        }
+        else{
+            printf("(<=slovo and flag=0) ");
+        }
+        p=p->next;
     }
-	for(i=0;i<20;i++){
-		if(p->str[i]!=0){
-			putchar(p->str[i]);
-		}
-		else{
-			//putchar(' ');
-			break;
-		}
-	}
-	if(p->flag==1){
-		printf("(<=slovo and flag=1) ");
-	}
-	else{
-		printf("(<=slovo and flag=0) ");
-	}
+    for(i=0;i<20;i++){
+        if(p->str[i]!=0){
+            putchar(p->str[i]);
+        }
+        else{
+            //putchar(' ');
+            break;
+        }
+    }
+    if(p->flag==1){
+        printf("(<=slovo and flag=1) ");
+    }
+    else{
+        printf("(<=slovo and flag=0) ");
+    }
 //	putchar(0);
 }
